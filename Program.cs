56cf9304192c4842d8d786fe5c0da1652e53e298
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-11 11:47:00 +0400

    arrays

--- Program.cs
+++ Program.cs
@@ -6,45 +6,59 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            string []ae =new string [5];
-            ae[0] = "aaddel";
-            ae[1] = "nasser";
-            ae[2] = "subaiieh";
-            ae[3] = "allesawed";
-            ae[4] = "alfazari";
+            /* string []ae =new string [5];
+             ae[0] = "aaddel";
+             ae[1] = "nasser";
+             ae[2] = "subaiieh";
+             ae[3] = "allesawed";
+             ae[4] = "alfazari";
 
-            for(int i=0; i<ae.Length; i++)
-            {
-                Console.WriteLine(ae[i]);
-            }
+             for(int i=0; i<ae.Length; i++)
+             {
+                 Console.WriteLine(ae[i]);
+             }
 
-            int[] a = {1,5,7,9,10};
-            int sum = 0;
-            for(int i=0; i<a.Length; i++) 
-            { 
-                
-                sum += a[i]; 
-            
-            }
-            Console.WriteLine(sum);
+             int[] a = {1,5,7,9,10};
+             int sum = 0;
+             for(int i=0; i<a.Length; i++) 
+             { 
 
+                 sum += a[i]; 
+
+             }
+             Console.WriteLine(sum);
+            */
+
+
+            average();
+            secondLargest();
+        }
+        static void average()
+        {
             /*Write a program that accepts an array of integers and calculates the average 
             of all the elements.*/
             Console.WriteLine("Write a program that accepts an array of integers and calculates the average of all the elements.");
             int[] marks = { 40, 67, 89, 56, 66, 90, 54, 78 };
-            int sum1 = 0;
-            for(int i= 0; i<marks.Length; i++)
+            double sum1 = 0;
+            for (int i = 0; i < marks.Length; i++)
             {
                 sum1 += marks[i];
             }
-            double average= sum1 / marks.Length;
-            Console.WriteLine("The averag of marks is = "+average);
+            double average = sum1 / marks.Length;
+            Console.WriteLine("The averag of marks is = " + average);
+        }
+
+        static void maxMin()
+        {
+
+
 
             Console.WriteLine("Write a program that accepts an array of integers and finds the maximum and minimum elements in the array");
             //Console.WriteLine("the maximum element is = "+marks.Max);
+            int[] marks = { 45,35,56,67,64,53,63 };
             int max = marks[0];
             int min = marks[0];
-            for (int i = 0; i < marks.Length ; i++)
+            for (int i = 0; i < marks.Length; i++)
             {
                 if (marks[i] > max)
                 {
@@ -55,26 +69,58 @@ namespace Arrays
                     min = marks[i];
                 }
             }
-            Console.WriteLine("The maximum value is = "+max);
-            Console.WriteLine("the minimum value is = "+min);
+            Console.WriteLine("The maximum value is = " + max);
+            Console.WriteLine("the minimum value is = " + min);
+        }
+
+        static void oddEven()
+        {
 
             Console.WriteLine("Write a program that accepts an array of integers and counts the number of even and odd elements.");
-
+            int[] marks = { 40, 67, 89, 56, 66, 90, 54, 78 };
             int evencounter = 0;
-            int oddcounter  = 0;
-            for (int i= 0;i<marks.Length ; i++)
+            int oddcounter = 0;
+            for (int i = 0; i < marks.Length; i++)
             {
-                if (marks[i] % 2==0) 
+                if (marks[i] % 2 == 0)
                 {
-                evencounter++;
+                    evencounter++;
                 }
-                if (marks[i] %2 !=0) 
-                { 
+                if (marks[i] % 2 != 0)
+                {
                     oddcounter++;
                 }
             }
             Console.WriteLine("The  total even number are = " + evencounter);
             Console.WriteLine("the total odd number  are  = " + oddcounter);
         }
+
+
+            static void secondLargest()
+            {
+                Console.WriteLine("Write a program that accepts an array of integers and finds the second largest element in the array.");
+
+                int [] values= { 12, 34, 77, 20, 45, 23, 22 };
+                int max = values[0];
+                int secondmax =values[0];
+
+                for(int i=0;i<values.Length ; i++) 
+                {
+                    if (values[i] > max)
+                    {
+                        secondmax = max;
+                        max = values[i];
+                    }
+                    else if (values[i] > secondmax)
+                    {
+                        secondmax = values[i];
+                    }
+                }
+                Console.WriteLine("The second largest element in the array is {0}", secondmax);
+
+            }
+
+
+        
     }
 }
